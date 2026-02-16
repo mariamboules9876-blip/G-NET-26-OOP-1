4c4565d3b1618587aca2d77e90a35cae3d9b4269
mariamboules9876-blip <mariamboules9876@gmail.com>
2026-02-15 23:16:15 -0800

    part1-Q1

--- app6/Program.cs
+++ app6/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //part1
+
+            //Q1
+//            using System;
+
+//class PersonClass
+//        {
+//            public string Name;
+//        }
+
+//        struct PersonStruct
+//        {
+//            public string Name;
+//        }
+
+//        class Program
+//        {
+//            static void Main()
+//            {
+//                // Class Example
+//                PersonClass p1 = new PersonClass();
+//                p1.Name = "Mariam";
+
+//                PersonClass p2 = p1;
+//                p2.Name = "Ahmed";
+
+//                Console.WriteLine("Class Output: " + p1.Name);
+//                // Ahmed → لأنه Reference Type
+
+//                // Struct Example
+//                PersonStruct s1;
+//                s1.Name = "Mariam";
+
+//                PersonStruct s2 = s1;
+//                s2.Name = "Ahmed";
+
+//                Console.WriteLine("Struct Output: " + s1.Name);
+//                // Mariam → لأنه Value Type
+//            }
+//        }
+
     }
+}
 }
